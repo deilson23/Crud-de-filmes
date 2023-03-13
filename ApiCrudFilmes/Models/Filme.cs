b734ba2702ea243baf0855f8c7d69a98703d669a
DeilsonNovaes <deilson2323@gmail.com>
2023-03-13 20:38:50 -0300

    Adicionado classe filme em model

---
+++ ApiCrudFilmes/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiCrudFilmes.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Idioma { get; set; }
+        public int Duracao { get; set; }
+        public float Avaliacao { get; set; }
+
+
+    }
+}
