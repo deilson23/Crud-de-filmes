86ecfa8c5c5a4380332e6587453ce6d275d58d3c
DeilsonNovaes <deilson2323@gmail.com>
2023-03-14 15:29:17 -0300

    Adicionado metodo post, get, getid

--- ApiCrudFilmes/Controllers/FilmeController.cs
+++ ApiCrudFilmes/Controllers/FilmeController.cs
@@ -1,8 +1,11 @@
 ﻿using ApiCrudFilmes.Data;
+using ApiCrudFilmes.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCrudFilmes.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
         private readonly Context _context;
@@ -10,6 +13,29 @@ namespace ApiCrudFilmes.Controllers
         {
             _context = context;
 
+        }
+        [HttpGet]
+        public IActionResult BuscaFilmes()
+        {
+            
+            return Ok(_context.Filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarFilmePorId(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id==id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult AddFilme([FromBody]Filme filme)
+        {
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(BuscarFilmePorId), new {Id = filme.Id}, filme);
         }
     }
 }
